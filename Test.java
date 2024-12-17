865ad7cdd01939260518ee5a80b0c161cf7f81f7
vjvj26 <57322956+vjvj26@users.noreply.github.com>
2024-12-17 15:45:27 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Geeks {
+
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using new keyword
+        String str = new String("GeeksforGeeks");
+
+        System.out.println(str);
+    }
+}
